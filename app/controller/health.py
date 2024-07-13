191e9b0f0524a4f3ca7b304f0854e84402d1bf11
wihlarkop <wihlarko26@gmail.com>
2024-07-14 03:41:40 +0700

    - fix health

--- app/controller/health.py
+++ app/controller/health.py
@@ -1,22 +1,23 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from sqlalchemy import select
 
 from app.config import settings
-from app.dependency.database import DBSession
+from app.dependency.database import DBConnection
+from app.dependency.exception import InternalServerError
 from app.helper.response import JsonResponse
 
 health_router = APIRouter(tags=["Health"])
 
 
 @health_router.get(path="/health")
-async def health_check(request: Request, session: DBSession) -> JsonResponse:
+async def health_check(request: Request, conn: DBConnection) -> JsonResponse:
     result = {
         "name": "Barbershop API",
         "version": settings.APP_VERSION,
         "status": {}
     }
 
-    db_health = await session.execute(select(1))
+    db_health = await conn.execute(select(1))
     health_result = db_health.scalar()
 
     if health_result == 1:
