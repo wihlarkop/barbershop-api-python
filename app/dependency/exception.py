9581feb1856f16d7fcc3961b10c315b88df3b791
wihlarkop <wihlarko26@gmail.com>
2024-07-14 03:41:50 +0700

    - fix exception

--- app/dependency/exception.py
+++ app/dependency/exception.py
@@ -1,25 +1,30 @@
-from fastapi import HTTPException, Request, status
-from fastapi.exceptions import RequestValidationError
+from typing import Callable
+
+from fastapi import Request
+from starlette.responses import JSONResponse
 
 from app.helper.response import JsonResponse
 
 
-async def handle_custom_http_exception(request: Request, exc: HTTPException):
-    message: str = exc.detail
-    status_code: int = exc.status_code
-
-    return JsonResponse(
-        message=message,
-        status_code=status_code,
-        data=None,
-        success=False
-    )
+class InternalServerError(Exception):
+    def __init__(self, message: str = None):
+        self.message = message
+        super().__init__(self.message)
 
 
-async def handle_custom_validation_error(request: Request, exc: RequestValidationError):
-    return JsonResponse(
-        data=f"value is {exc.body}",
-        message=exc.errors(),
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        success=False
-    )
+def create_exception_handler(
+        status_code: int, message: str
+) -> Callable[[Request, Exception], JsonResponse]:
+    async def exception_handler(_: Request, exc: Exception) -> JSONResponse:
+        error_message = message
+        if exc is not None and hasattr(exc, 'message') and exc.message:
+            error_message = exc.message
+
+        return JsonResponse(
+            message=error_message,
+            status_code=status_code,
+            data=None,
+            success=False
+        )
+
+    return exception_handler
